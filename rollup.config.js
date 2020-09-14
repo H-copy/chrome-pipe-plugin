cc6a96fceb0077c65bb82380ab5899cec2c8e276
胡海国 <huhaiguo@zkdntech.com>
2020-09-14 18:16:35 +0800

    cli 配置多类型包

--- rollup.config.js
+++ rollup.config.js
@@ -4,13 +4,13 @@ import commonjs from 'rollup-plugin-commonjs'; // commonjs模块转换插件
 import babel from "rollup-plugin-babel"; // babel 插件
 import { eslint } from 'rollup-plugin-eslint'; // eslint插件
 
-export default {
+const defaultConf = {
 
   input: './src/main.js', // 打包的入口文件
   output:{
     name: 'my-pkg',  // 输入的包名
     file: './lib/index.js', // 打包输出地址, 这里的导出地址就是package内main的地址
-    format: 'umd' // 包类型
+    format: 'es' // 包类型
   },
   plugins: [  // 使用的插件
     resolve(),
@@ -30,3 +30,27 @@ export default {
   ]
   
 }
+
+const buildConf = option =>{
+  return Object.assign({}, defaultConf, option)
+}
+
+export default [
+
+  buildConf({
+    output: {
+      name: 'my-pkg',  
+      file: './bin/index.js',
+      format: 'es'
+    }
+  }),
+
+  buildConf({
+    output: {
+      name: 'my-pkg',  
+      file: './lib/index.js',
+      format: 'umd'
+    }
+  })
+  
+]
